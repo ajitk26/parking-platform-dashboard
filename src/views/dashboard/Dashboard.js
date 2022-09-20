dab2785b03d81236f5e370f3f7522d07a0ef2451
anirudhp26 <anirudh26p@gmail.com>
2022-09-20 19:54:07 +0530

    dashboard-layout-added

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,7 +1,57 @@
 import React from 'react'
+import './dashboard.css'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <div className="root">
+        <div className="pane-1">
+          <div className="p1-section-1">
+            <div className="p1-section-1-1">
+              <div className="p1-section-1-1-1">
+                <p>Summary Analytics</p>
+                <p>Traffic flow Analytics</p>
+              </div>
+              <div className="p1-section-1-1-2"></div>
+            </div>
+            <div className="p1-section-1-2">
+              <div className="p1-section-1-2-1">
+                <div className="p1-section-1-2-1-1">
+                  <p>To do list</p>
+                </div>
+              </div>
+              <div className="p1-section-1-2-2">
+                <div className="p1-section-1-2-2-1">
+                  <p>Vehicle on Feild</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p1-section-2"></div>
+          <div className="p1-section-3">
+            <div className="p1-section-1-1-1">
+              <p>Summary</p>
+              <p>Payment</p>
+              <p>Vehicle Type</p>
+              <p>Vehicle Auth</p>
+            </div>
+          </div>
+        </div>
+        <div className="pane-2">
+          <div className="p2-section-1">
+            <div className="p1-section-1-1-1">
+              <p>Bulletin</p>
+              <p>Solution</p>
+              <p>Order</p>
+            </div>
+          </div>
+          <div className="p2-section-2"></div>
+          <div className="p2-section-3"></div>
+          <div className="p2-section-4"></div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
