ee955039b73e458602bb6d8d8e78076965313b00
anirudhp26 <anirudh26p@gmail.com>
2022-09-22 01:29:44 +0530

    Update ParkingList.js

--- src/views/configuration/parkingList/ParkingList.js
+++ src/views/configuration/parkingList/ParkingList.js
@@ -33,7 +33,7 @@ const ParkingList = () => {
       _props: { scope: 'col' },
     },
     {
-      key: 'opration',
+      key: 'operation',
       label: 'Operation',
       _props: { scope: 'col' },
     },
@@ -41,23 +41,13 @@ const ParkingList = () => {
   const items = [
     {
       id: 1,
-      class: 'Mark',
-      heading_1: 'Otto',
-      heading_2: '@mdo',
+      parkingName: 'Mark',
+      systemType: 'Otto',
+      account: '@mdo',
+      lastonline: '10:00 AM',
+      network: 'gtpl',
+      operation: 'ok',
       _cellProps: { id: { scope: 'row' } },
-    },
-    {
-      id: 2,
-      class: 'Jacob',
-      heading_1: 'Thornton',
-      heading_2: '@fat',
-      _cellProps: { id: { scope: 'row' } },
-    },
-    {
-      id: 3,
-      class: 'Larry the Bird',
-      heading_2: '@twitter',
-      _cellProps: { id: { scope: 'row' }, class: { colSpan: 2 } },
     },
   ]
   return (
