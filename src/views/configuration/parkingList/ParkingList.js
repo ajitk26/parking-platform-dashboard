e3ce57ffef4b446a877a165083944b8956cfb911
anirudhp26 <anirudh26p@gmail.com>
2022-09-21 20:07:43 +0530

    parking list edited

--- src/views/configuration/parkingList/ParkingList.js
+++ src/views/configuration/parkingList/ParkingList.js
@@ -1,7 +1,73 @@
 import React from 'react'
-
+import { CTable } from '@coreui/react'
 const ParkingList = () => {
-  return <div>ParkingList</div>
+  const columns = [
+    {
+      key: 'id',
+      label: 'No.',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'parkingName',
+      label: 'Parking Name',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'systemType',
+      label: 'System Type',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'account',
+      label: 'Account',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'lastonline',
+      label: 'Last Time Online',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'network',
+      label: 'Network',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'opration',
+      label: 'Operation',
+      _props: { scope: 'col' },
+    },
+  ]
+  const items = [
+    {
+      id: 1,
+      class: 'Mark',
+      heading_1: 'Otto',
+      heading_2: '@mdo',
+      _cellProps: { id: { scope: 'row' } },
+    },
+    {
+      id: 2,
+      class: 'Jacob',
+      heading_1: 'Thornton',
+      heading_2: '@fat',
+      _cellProps: { id: { scope: 'row' } },
+    },
+    {
+      id: 3,
+      class: 'Larry the Bird',
+      heading_2: '@twitter',
+      _cellProps: { id: { scope: 'row' }, class: { colSpan: 2 } },
+    },
+  ]
+  return (
+    <>
+      <div className="root">
+        <h1>ParkingList</h1>
+        <CTable columns={columns} items={items} />
+      </div>
+    </>
+  )
 }
 
 export default ParkingList
