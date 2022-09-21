867fcdfcb246e42611376609e75eacb695d5e560
Ajit kumar <aky8062@gmail.com>
2022-09-21 11:04:46 +0530

    updated

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -37,11 +37,6 @@ const AppHeader = () => {
         </CHeaderBrand> */}
         <CHeaderNav className="d-none d-md-flex me-auto">
           <CNavItem>
-            <CNavLink to="/dashboard" component={NavLink}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
             <CDropdown>
               <CDropdownToggle color="white">Services</CDropdownToggle>
               <CDropdownMenu>
