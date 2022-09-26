e1ee69d3c60685e5012db6b7c913fe75e8c338f9
anirudhp26 <anirudh26p@gmail.com>
2022-09-26 15:08:31 +0530

    ci errors resolved

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
+import { CSidebar, CSidebarNav, CSidebarToggler } from '@coreui/react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
