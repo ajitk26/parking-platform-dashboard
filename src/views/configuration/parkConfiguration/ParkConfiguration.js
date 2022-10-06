df9a8064bd3f14e4bce15b9c2a4b91a3c8521a8f
Ajit kumar <aky8062@gmail.com>
2022-10-06 15:10:18 +0530

    dashboard complete

--- src/views/configuration/parkConfiguration/ParkConfiguration.js
+++ src/views/configuration/parkConfiguration/ParkConfiguration.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { CFormSelect } from '@coreui/react'
 
 const ParkConfiguration = () => {
-  return <div>ParkConfiguration</div>
+  return (
+    <div>
+      <h2>Sub Configuration</h2>
+      <div>
+        <CFormSelect aria-label="Default select example" style={{ width: '25%' }}>
+          <option>Mall</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </CFormSelect>
+      </div>
+    </div>
+  )
 }
 
 export default ParkConfiguration
