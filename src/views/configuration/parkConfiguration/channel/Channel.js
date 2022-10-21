75b5e91af37e33240515137ed69896270e105bcf
Ajit kumar <aky8062@gmail.com>
2022-10-21 15:20:15 +0530

    updated

--- src/views/configuration/parkConfiguration/channel/Channel.js
+++ src/views/configuration/parkConfiguration/channel/Channel.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import { CCallout, CRow, CCol, CFormLabel, CFormInput, CFormSelect } from '@coreui/react'
 
 const Channel = () => {
-  return <div>Channel</div>
+  return (
+    <div>
+      <div style={{ marginBottom: 13 }}>
+        <Button variant="contained">Submit</Button>
+      </div>
+      <h5>Create Channel</h5>
+      <CCallout color="danger">
+        <b>Tips :</b> Both handheld and cashier belong to cashier equipment
+      </CCallout>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Channel Name :</CFormLabel>
+        <CCol sm={4}>
+          <CFormInput type="text" />
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Rel-Cashier:</CFormLabel>
+        <CCol sm={4}>
+          <CFormSelect aria-label="Default select example">
+            <option>--Choose--</option>
+          </CFormSelect>
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Property :</CFormLabel>
+        <CCol sm={4}>
+          <CFormSelect aria-label="Default select example">
+            <option>Entry</option>
+            <option>Exit</option>
+            <option>All in one</option>
+          </CFormSelect>
+        </CCol>
+      </CRow>
+    </div>
+  )
 }
 
 export default Channel
--- src/views/configuration/parkConfiguration/discounter/Discounter.js
+++ src/views/configuration/parkConfiguration/discounter/Discounter.js
@@ -1,7 +1,51 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import { CRow, CFormLabel, CFormSelect, CCol, CFormInput } from '@coreui/react'
 
 const Discounter = () => {
-  return <div>Discounter</div>
+  return (
+    <div>
+      <div style={{ marginBottom: 13 }}>
+        <Button variant="contained">Submit</Button>
+      </div>
+      <h5>Create Discounter</h5>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Device Type :</CFormLabel>
+        <CCol sm={4}>
+          <CFormSelect aria-label="Default select example">
+            <option>Merchant Machine</option>
+            <option>Self-Service Machine</option>
+          </CFormSelect>
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Code :</CFormLabel>
+        <CCol sm={4}>
+          <CFormSelect aria-label="Default select example">
+            <option>--Choose--</option>
+          </CFormSelect>
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Name :</CFormLabel>
+        <CCol sm={4}>
+          <CFormInput type="text" />
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">IP :</CFormLabel>
+        <CCol sm={4}>
+          <CFormInput type="text" defaultValue="192 . 168 . 8 . 99" />
+        </CCol>
+      </CRow>
+      <CRow className="mb-3">
+        <CFormLabel className="col-sm-2 col-form-label">Gateway :</CFormLabel>
+        <CCol sm={4}>
+          <CFormInput type="text" defaultValue="192 . 168 . 8 . 1" />
+        </CCol>
+      </CRow>
+    </div>
+  )
 }
 
 export default Discounter
